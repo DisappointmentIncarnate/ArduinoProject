c3f79472f87e7e3207e98cc6a2ed9b2abe4b62f3
DisappointmentIncarnate <112603794+DisappointmentIncarnate@users.noreply.github.com>
2024-04-02 17:37:40 -0400

    reads and outputs potentiometer

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,9 +1,26 @@
-void setup() {
-  // put your setup code here, to run once:
 
+// INPUT: Potentiometer should be connected to 5V and GND
+int potPin = A3; // Potentiometer output connected to analog pin 3
+int potVal = 0; // Variable to store the input from the potentiometer
+
+bool running = true; //flag used to continue the program
+
+void setup()
+{
+  Serial.begin(9600); //baud output
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+// Main program
+void loop()
+{
+  if(running){ //continue running
+    potVal = analogRead(potPin);   // read the potentiometer value at the input pin
 
+    Serial.println(potVal); //literally just print the value out
+    delay(1);
+
+    if(potVal == 0 ){
+      running = false;
+    }
+  }
 }
