50ce838ca5f9b1e5163f5ffb51eed7265dbd83bd
DisappointmentIncarnate <112603794+DisappointmentIncarnate@users.noreply.github.com>
2024-04-08 23:40:43 -0400

    yea button

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -10,19 +10,24 @@ int enemy3_location[] = {730, 770};
 // INPUT: Potentiometer should be connected to 5V and GND
 int potPin = A3; // Potentiometer output connected to analog pin 3
 int potVal = 0; // Variable to store the input from the potentiometer
+int buttonPin = 7;
+
 
 bool running = true; //flag used to continue the program
-bool enemy1_on = false;
-bool enemy2_on = false;
-bool enemy3_on = false;
+bool enemy1_on; 
+bool enemy2_on; 
+bool enemy3_on;
 
 int randNumber;
+int buttonState;
 
 int timer = 2000;
 
 void setup()
 {
   Serial.begin(9600); //baud output
+  startup_motors();
+  pinMode(buttonPin, INPUT);
 
   // if analog input pin 0 is unconnected, random analog
   // noise will cause the call to randomSeed() to generate
@@ -30,10 +35,6 @@ void setup()
   // randomSeed() will then shuffle the random function.
   randomSeed(analogRead(0));
 
-  //Servo motor attaching
-  //enemy1.attach(1);
-  //enemy2.attach(2);
-  //enemy3.attach(3);
 }
 
 // Main program
@@ -61,6 +62,31 @@ void loop()
       running = false;
     }
   }
+  buttonPush();
+}
+
+void startup_motors(){
+  //Servo motor attaching
+  enemy1.attach(2);
+  enemy2.attach(3);
+  enemy3.attach(4);
+  //set position back to 0
+  enemy1.write(0);
+  enemy2.write(0);
+  enemy3.write(0);
+  //write flags
+  enemy1_on = false;
+  enemy2_on = false;
+  enemy3_on = false;
+}
+
+void buttonPush(){
+  buttonState = digitalRead(buttonPin);
+  if(buttonState == HIGH){
+    Serial.println("BUTTON PUSHED");
+  }else{
+    Serial.println("BUTTON NOT PUSHED");
+  }
 }
 
 void extendServo(int num, int potVal){
@@ -68,17 +94,17 @@ void extendServo(int num, int potVal){
   if(num == 1 && !enemy1_on && currPos != 1){
     //servo 1
     Serial.println("Enemy 1 Extend");
-    //enemy1.write(180);
+    enemy1.write(180);
     enemy1_on = true;
   }else if(num == 2 && !enemy2_on && currPos != 2){
     //servo 2
     Serial.println("Enemy 2 Extend");
-    //enemy2.write(180);
+    enemy2.write(180);
     enemy2_on = true;
   }else if(num == 3 && !enemy3_on && currPos != 3){
     //servo 3
     Serial.println("Enemy 3 Extend");
-    //enemy3.write(180);
+    enemy3.write(180);
     enemy3_on = true;
   }else{
     //Serial.println(num);
@@ -87,15 +113,15 @@ void extendServo(int num, int potVal){
 
 void retractServo(int position){
   if(enemy1_on && position == 1){
-    //enemy1.write(0);
+    enemy1.write(0);
     enemy1_on = false;
     Serial.println("Enemy 1 hit");
   }else if(enemy2_on && position == 2){
-    //enemy2.write(0);
+    enemy2.write(0);
     enemy2_on = false;
     Serial.println("Enemy 2 hit");
   }else if(enemy3_on && position == 3){
-    //enemy3.write(0);
+    enemy3.write(0);
     enemy3_on = false;
     Serial.println("Enemy 3 hit");
   }
